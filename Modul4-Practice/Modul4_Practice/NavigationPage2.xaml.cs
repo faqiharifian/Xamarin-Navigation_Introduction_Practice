c601fe3df7c3133b2b78c7626565bf16ce47454a
Faqih Arifian <faqiharifianaji@gmail.com>
2017-06-10 13:51:05 +0700

    practice 1 simple page navigation

---
+++ Modul4-Practice/Modul4_Practice/NavigationPage2.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Modul4_Practice
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NavigationPage2 : ContentPage
+	{
+		public NavigationPage2 ()
+		{
+			InitializeComponent ();
+
+            btnBack.Clicked += async (sender, e) =>
+            {
+                await Navigation.PopAsync(true);
+            };
+		}
+	}
+}
